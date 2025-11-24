f11d38d3127b72427ab16f0e748499a4eaa8ac3a
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-24 15:23:38 +0100

    Adding a function that return the sum of all the float values of a list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+'''5-sum_list.py Script that returns the sum of of a list'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''function sum_list which takes a list input_list of floats as argument
+    and returns their sum as a float.'''
+    return sum(input_list)
