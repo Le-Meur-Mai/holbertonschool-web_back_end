4099b3720319547083401679f8ff9d65d3a3e680
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-24 14:21:07 +0100

    Fix pycodestyle and comments

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-''' Script that use python's annotations '''
+'''Script that use python's annotations'''
 
-def add(a : float, b : float) -> float :
-    ''' Function that add two floats and return their sum in float type'''
+
+def add(a: float, b: float) -> float:
+    '''Function that add two floats and return their sum in float type'''
     return (a + b)
