95a3f6d3223d9082fe8d97e138564411a7e1234b
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-24 14:59:34 +0100

    Adding python's annotations to variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+'''4-define_variables.py Define and annotate
+the following variables with specifics values:'''
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
