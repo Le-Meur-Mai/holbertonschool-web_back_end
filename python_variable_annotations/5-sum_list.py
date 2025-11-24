c6104970bc26e4b43642559e142a58918ec1f1a1
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-24 15:29:48 +0100

    Adding an old syntaxe

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+from typing import List
+
 '''5-sum_list.py Script that returns the sum of of a list'''
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     '''function sum_list which takes a list input_list of floats as argument
     and returns their sum as a float.'''
     return sum(input_list)
