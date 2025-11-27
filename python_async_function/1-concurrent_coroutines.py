a387accdcafd2fba1bb76a02a7a5c7d7c08e9341
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-27 13:38:44 +0100

    Adding an asynchronous function that start multiple coroutines

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+'''1-concurrent_coroutines.py'''
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    '''Function that has an async routine called wait_n that takes in 2 int
+    arguments (in this order): n and max_delay. You will spawn wait_random n
+    times with the specified max_delay.'''
+
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    '''Renvoie une liste d'objet de type coroutine qui ne sont pas lancés
+    mais en attente, t'en que les fonction asynchrone ne sont pas lancées par
+    une boucle d'évenements comme as_completed ou gather'''
+    list_result = []
+    for task in asyncio.as_completed(tasks):
+        '''as_completed lance les coroutine en même temps et attends qu'une
+        tache se termine sans attendre les autres pourla mettre dans la liste
+        à la différence de gather qui prend des arguments et renvoie les
+        résultats dans l'ordre des arguments.'''
+        result = await task
+        list_result.append(result)
+    return list_result
