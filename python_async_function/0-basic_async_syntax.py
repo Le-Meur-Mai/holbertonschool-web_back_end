6523090defc2024f8b6d464bffd710c9dfed7783
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-27 10:05:34 +0100

    Adding typing

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     '''asynchronous coroutine that takes in an integer argument
     (max_delay, with a default value of 10) named wait_random that waits for a
     random delay between 0 and max_delay (included and float value) seconds and
