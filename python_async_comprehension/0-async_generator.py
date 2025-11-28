de9ce2cd66172cc2615fb2370f935e070b363cb6
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-28 15:50:48 +0100

    Adding an asynchronous function that yield a random number te time

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''0-async_generator.py'''
+
+import asyncio
+import random
+
+
+async def async_generator():
+    '''A coroutine called async_generator that takes no arguments.
+    The coroutine will loop 10 times, each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10. Use the random module.'''
+
+    for i in range(0, 10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
