9b6b41e43554dd4626a7351db9745a3951280eeb
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-08 16:13:38 +0100

    Modification of the syntax

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,8 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = {income, gdp, capita};
+  const budget = {};
+  budget.income = income;
+  budget.gdp = gdp;
+  budget.capita = capita;
+
   return budget;
 }
