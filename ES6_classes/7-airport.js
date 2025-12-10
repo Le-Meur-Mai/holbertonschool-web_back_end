abc4a4f394db27e3ad1a2aacf8ccd9d78e9ca40a
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-11 00:30:06 +0100

    Addinga class that use a a default string description

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,36 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name != "string" || typeof code != "string") {
+      throw TypeError("name and code must be a string");
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value != "string") {
+      throw TypeError("name and code must be a string");
+    }
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value != "string") {
+      throw TypeError("name and code must be a string");
+    }
+    this._code = value;
+  }
+
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
