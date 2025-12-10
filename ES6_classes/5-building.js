a1d2994aa5e997255c0e82cab82ad97ba38fb521
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-10 18:57:23 +0100

    Adding an abstract method

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,27 @@
+export default class Building {
+  constructor (sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === 
+      Building.prototype.evacuationWarningMessage) {
+      throw new Error("Class extending Building must override evacuationWarningMessage");
+      }
+    if (typeof sqft != "number") {
+      throw TypeError("sqft must be a number");
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value != "number") {
+      throw TypeError("sqft must be a number");
+    }
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    throw Error("Class extending Building must override evacuationWarningMessage");
+  }
+}
