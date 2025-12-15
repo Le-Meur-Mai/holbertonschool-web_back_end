b2830b69fc93c1503a350fbaa2dbc912cc432661
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-15 14:24:56 +0100

    Correcting the wrong id

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,4 @@
-export default function getListStudents() {
-  return ([
+export default () => [
   {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-  {id: 2, firstName: 'James', location: 'Columbia'},
-  {id: 3, firstName: 'Serena', location: 'San Francisco'}]);
-}
+  {id:2, firstName: 'James', location: 'Columbia'},
+  {id: 5, firstName: 'Serena', location: 'San Francisco'}];
