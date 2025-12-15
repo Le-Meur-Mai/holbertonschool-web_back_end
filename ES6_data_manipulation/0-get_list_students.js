696d863dfe20f0246661c9bfd2317147c96c9577
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-15 14:18:30 +0100

    Adding a function in ES6 format that return an array of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,4 @@
+export default () => [
+  {id: 1,firstName: 'Guillaume', location: 'San Francisco'},
+  {id:2, firstName: 'James', location: 'Columbia'},
+  {id: 3, firstName: 'Serena', location: 'San Francisco'}];
