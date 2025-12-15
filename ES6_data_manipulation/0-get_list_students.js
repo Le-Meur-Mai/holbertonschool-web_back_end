b098e9749da2bd672ffac81f44c1199c3ce54f04
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-15 14:23:32 +0100

    Modification of the syntax in a non ES6 way for the checker

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,6 @@
-export default () => [
-  {id: 1,firstName: 'Guillaume', location: 'San Francisco'},
-  {id:2, firstName: 'James', location: 'Columbia'},
-  {id: 3, firstName: 'Serena', location: 'San Francisco'}];
+export default function getListStudents() {
+  return ([
+  {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+  {id: 2, firstName: 'James', location: 'Columbia'},
+  {id: 3, firstName: 'Serena', location: 'San Francisco'}]);
+}
