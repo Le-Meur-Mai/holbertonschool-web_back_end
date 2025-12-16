c27550e54c8ae2e5116fb0d7259f6fab782a0fa1
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-16 15:52:07 +0100

    Modification of the syntax

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.finally(() => console.log("Got a response from the API"));
-  promise.then({'status': 200, 'body': 'Success'});
-  promise.catch(new Error);
+  return promise
+  .then(() => ({'status': 200, 'body': 'Success'}))
+  .catch(() => new Error)
+  .finally(() => console.log("Got a response from the API"));
 }
