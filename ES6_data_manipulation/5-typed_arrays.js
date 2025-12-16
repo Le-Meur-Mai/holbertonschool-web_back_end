0d153b525afe907b91995a51345209b523ca9b17
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-16 09:55:06 +0100

    Redoing th error handle

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error("Position outside range");
+  }
   let buffer = new ArrayBuffer(length);
   let view = new Uint8Array(buffer);
-  try {
-    view[position] = value;
-  }
-  catch (error) {
-    console.log("Position outside range");
-  }
+  view[position] = value;
   let newArray = new DataView(buffer);
   return newArray;
 }
