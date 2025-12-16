1278034906c393ad9d77ddc4e51eb2f9c4b8589b
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-16 15:27:37 +0100

    Adding a function that return a promise with a resolve result or reject result

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+export default function getFullResponseFromAPI(success) {
+  let promise = new Promise(function(resolve, reject) {
+    if (success === true) {
+      resolve({'status': 200,
+        'body': 'Success'
+      });
+    }
+    else {
+      reject(new Error("The fake API is not working currently"));
+    }
+  });
+
+  promise.then();
+  return promise;
+}
