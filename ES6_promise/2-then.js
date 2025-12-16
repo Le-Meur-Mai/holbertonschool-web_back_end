77b0f63bb70526462f41217ba6324335ff13e4f9
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-16 15:45:40 +0100

    Modification of the syntax of resolve and reject

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
   promise.finally(() => console.log("Got a response from the API"));
-  promise.then(() => ({'status': 200, 'body': 'Success'}));
-  promise.catch(() => new Error);
+  promise.then({'status': 200, 'body': 'Success'});
+  promise.catch(new Error);
 }
