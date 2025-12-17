8569d57845f50fc6fcb433f2e5aa967d3609c315
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-17 16:50:54 +0100

    Modification of the return syntax

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,11 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default async function handleProfileSignup (firstName, lastName, fileName) {
-  const promises = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  return promises;
+  const promises = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return promises.map((result) => {
+    if (result.status !== 'fulfilled') {
+      result.value = result.reason;
+      delete result.reason;
+    }
+  });
 }
