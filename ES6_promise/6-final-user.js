efbc13f17b57e12290949bee9d88fd52461b0fb6
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-17 17:02:30 +0100

    deelting a useless line of code

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,10 +3,9 @@ import uploadPhoto from './5-photo-reject.js';
 
 export default async function handleProfileSignup (firstName, lastName, fileName) {
   const promises = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  return promises.map((result) => {
+  return promises.map(result => {
     if (result.status !== 'fulfilled') {
       result.value = result.reason;
-      delete result.reason;
     }
     return {'status': result.status, 'value': result.value};
   });
