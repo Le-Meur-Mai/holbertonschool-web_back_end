667c5d218983045b0f6b553c030a5448e402e035
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-17 16:35:37 +0100

    deleting await

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,6 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default async function handleProfileSignup (firstName, lastName, fileName) {
-  const promises = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  const promises = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
   return promises;
 }
