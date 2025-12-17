1f5adf1843b7e069e0beefa5b6678c53d74676a0
Le-Meur-Mai <mai.lm@outlook.fr>
2025-12-17 17:07:21 +0100

    Converting object error to string

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,12 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const results = await Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
-
-  return results.map(res => ({
-    status: res.status,
-    value: res.status === 'fulfilled' ? res.value : res.reason,
-  }));
+export default async function handleProfileSignup (firstName, lastName, fileName) {
+  const promises = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return promises.map(result => {
+    if (result.status !== 'fulfilled') {
+      result.value = String(result.reason);
+    }
+    return {'status': result.status, 'value': result.value};
+  });
 }
