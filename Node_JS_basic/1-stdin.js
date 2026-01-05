ed476c3d06b745b27ccbebfa44d176bbba84aec1
Le-Meur-Mai <mai.lm@outlook.fr>
2026-01-05 16:10:41 +0100

    Adding a file that allow the user to input their name in the console

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+  const name = String(data).trim();
+  console.log(`Your name is: ${name}`);
+  process.on('beforeExit', () => {
+    console.log('This important software is now closing');
+  });
+});
