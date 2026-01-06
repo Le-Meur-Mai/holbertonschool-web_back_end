9a16026085c48ee5a2a668920c50e4503fb74139
Le-Meur-Mai <mai.lm@outlook.fr>
2026-01-06 11:10:32 +0100

    Code correction at line 5 that close the input process

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,6 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = String(data).trim();
   console.log(`Your name is: ${name}`);
+  process.stdin.end();
 });
 process.on('beforeExit', () => {
   console.log('This important software is now closing');
