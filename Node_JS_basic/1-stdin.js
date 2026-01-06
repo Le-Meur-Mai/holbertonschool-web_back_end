321e4b2427ea8abd233d50ada849b06ce727de84
Le-Meur-Mai <mai.lm@outlook.fr>
2026-01-06 10:53:13 +0100

    Part of the code was at the wrong place

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = String(data).trim();
   console.log(`Your name is: ${name}`);
-  process.on('beforeExit', () => {
+});
+process.on('beforeExit', () => {
     console.log('This important software is now closing');
   });
-});
