0725c3dd2ec72cd9f695037f286d736e9f979870
Le-Meur-Mai <mai.lm@outlook.fr>
2026-01-06 12:00:20 +0100

    Replacing console.log by process.stdoutt.write

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  process.exit(0);
+  const name = String(data).trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
